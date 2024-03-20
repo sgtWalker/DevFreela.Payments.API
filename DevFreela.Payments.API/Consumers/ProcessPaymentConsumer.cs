99088b25040632b2418c1189579c441711bd88c9
Diego Araujo <diego.araujo@totvs.com.br>
2024-03-20 20:33:20 -0300

    Refatoração para utilização do rabbitMQ para processar os pagamentos

---
+++ DevFreela.Payments.API/Consumers/ProcessPaymentConsumer.cs
@@ -0,0 +1,74 @@
+﻿
+using DevFreela.Payments.API.Models;
+using DevFreela.Payments.API.Services;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+namespace DevFreela.Payments.API.Consumers
+{
+    public class ProcessPaymentConsumer : BackgroundService
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private readonly IServiceProvider _serviceProvider;
+        private const string PAYMENTS_QUEUE = "payments";
+        private const string PAYMENTS_APPROVED_QUEUE = "paymentsApproved";
+
+        public ProcessPaymentConsumer(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: PAYMENTS_QUEUE, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            _channel.QueueDeclare(queue: PAYMENTS_APPROVED_QUEUE, durable: false, exclusive: false, autoDelete: false, arguments: null);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var consumer = new EventingBasicConsumer(_channel);
+
+            consumer.Received += (sender, eventArgs) =>
+            {
+                var byteArray = eventArgs.Body.ToArray();
+                var paymentInfoJson = Encoding.UTF8.GetString(byteArray);
+                var paymentInfo = JsonSerializer.Deserialize<PaymentInfoInputModel>(paymentInfoJson);
+
+                ProcessPayment(paymentInfo);
+
+                PublishPaymentApproved(paymentInfo);
+
+                _channel.BasicAck(eventArgs.DeliveryTag, false);
+            };
+
+            _channel.BasicConsume(PAYMENTS_QUEUE, false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        private void PublishPaymentApproved(PaymentInfoInputModel paymentInfo)
+        {
+            var paymentApproved = new PaymentApprovedIntegrationEvent(paymentInfo.ProjectId);
+            var paymentApprovedJson = JsonSerializer.Serialize(paymentApproved);
+            var paymentApprovedBytes = Encoding.UTF8.GetBytes(paymentApprovedJson);
+
+            _channel.BasicPublish(exchange: "", routingKey: PAYMENTS_APPROVED_QUEUE, basicProperties: null, body: paymentApprovedBytes);
+        }
+
+        public void ProcessPayment(PaymentInfoInputModel paymentInfo)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var paymentService = scope.ServiceProvider.GetRequiredService<IPaymentService>();
+                paymentService.Process(paymentInfo);
+            };
+        }
+    }
+}
---
+++ DevFreela.Payments.API/Models/PaymentApprovedIntegrationEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace DevFreela.Payments.API.Models
+{
+    public class PaymentApprovedIntegrationEvent
+    {
+        public PaymentApprovedIntegrationEvent(Guid projectId)
+        {
+            ProjectId = projectId;
+        }
+
+        public Guid ProjectId { get; set; }
+    }
+}
